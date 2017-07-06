2cffed9019e737d672627f81c3d49ebcf277b844
Infi-Knight <soniravi829@gmail.com>
2017-07-06 13:09:09 +0530

    add mongoose body-parser, created models

---
+++ server/db/mongoose.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+// We will use the builtin Promise library
+mongoose.Promise = global.Promise;
+// `open()` is deprecated in mongoose >= 4.11.0, use `openUri()` instead,
+// or set the `useMongoClient` option if using `connect()` or `createConnection()`.
+// See http://mongoosejs.com/docs/connections.html#use-mongo-client
+mongoose.connect('mongodb://localhost:27017/TodoApp', {
+  useMongoClient: true
+});
