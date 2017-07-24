acb7bd0da937792935a5042ff32045a65e3008fb
Infi-Knight <soniravi829@gmail.com>
2017-07-24 22:12:46 +0530

    update app to use config variables and config.json

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,14 @@
+// When you require a json it is automatically parsed into a javascript object
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+	var config = require('./config.json');
+	// You need to use the bracket notation when accessing properties through variables
+	var envConfig = config[env];
+	// Object.keys takes an object and returns its properties as an array 
+	Object.keys(envConfig).forEach((key) => {
+		process.env[key] = envConfig[key];
+	});
+	// console.log(Object.keys(envConfig));
+	// console.log(config);
 }
--- server/models/user.js
+++ server/models/user.js
@@ -48,7 +48,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   user.tokens.push({access, token});
 
@@ -63,7 +63,7 @@ UserSchema.statics.findByToken = function (token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     // // jwt throws an error if the authtoken was not valid
     // // so if we reach this point we know that something is not wrong
